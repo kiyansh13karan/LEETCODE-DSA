74fdcb5ca1800af5a2fbfeef077b4c5a5969c506
Karan Nayal <138805592+kiyansh13karan@users.noreply.github.com>
2026-02-20 12:50:02 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (98.43%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        if(s.size() < t.size()) return "";
+        
+        vector<int> freq(128, 0);
+        for(char c : t) freq[c]++;
+        
+        int left = 0, right = 0;
+        int minLen = INT_MAX, start = 0;
+        int required = t.size();
+        
+        while(right < s.size()) {
+            if(freq[s[right]] > 0)
+                required--;
+            
+            freq[s[right]]--;
+            right++;
+            
+            while(required == 0) {
+                if(right - left < minLen) {
+                    minLen = right - left;
+                    start = left;
+                }
+                
+                freq[s[left]]++;
+                if(freq[s[left]] > 0)
+                    required++;
+                
+                left++;
+            }
+        }
+        
+        return minLen == INT_MAX ? "" : s.substr(start, minLen);
+    }
+};
