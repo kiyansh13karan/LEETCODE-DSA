c0764f28c521ac648f969760313fddb105a51917
Karan Nayal <138805592+kiyansh13karan@users.noreply.github.com>
2026-02-22 09:19:12 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (70.75%) - LeetSync

---
+++ 899-binary-gap/binary-gap.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        int last = -1, ans = 0, pos = 0;
+
+        while (n > 0) {
+            if (n & 1) {
+                if (last != -1)
+                    ans = max(ans, pos - last);
+                last = pos;
+            }
+            n >>= 1;
+            pos++;
+        }
+
+        return ans;
+    }
+};
