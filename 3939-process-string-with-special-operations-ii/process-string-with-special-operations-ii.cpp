278609add411f8536f8062b93851ab2608d60c58
Karan Nayal <138805592+kiyansh13karan@users.noreply.github.com>
2026-06-17 09:21:22 +0530

    Time: 19 ms (94.25%) | Memory: 24.1 MB (28.74%) - LeetSync

---
+++ 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    char processStr(string s, long long k) {
+        long long len = 0;
+
+        // Final length calculate karo
+        for (char c : s) {
+            if (c == '*') {
+                len = max(0LL, len - 1);
+            }
+            else if (c == '#') {
+                len *= 2;
+            }
+            else if (c == '%') {
+                continue;
+            }
+            else {
+                len++;
+            }
+        }
+
+        if (k >= len) return '.';
+
+        // Reverse traverse
+        for (int i = s.size() - 1; i >= 0; i--) {
+            char c = s[i];
+
+            if (c == '*') {
+                len++;
+            }
+            else if (c == '#') {
+                len /= 2;
+                if (k >= len) k -= len;
+            }
+            else if (c == '%') {
+                k = len - 1 - k;
+            }
+            else {
+                len--;
+                if (k == len) return c;
+            }
+        }
+
+        return '.';
+    }
+};
