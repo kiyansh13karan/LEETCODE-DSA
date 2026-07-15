dd8838592c17aff55ad66e9f7e38e2c1ef879fc5
Karan Nayal <138805592+kiyansh13karan@users.noreply.github.com>
2026-07-15 16:34:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (85.02%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -3,6 +3,6 @@ public:
     int gcdOfOddEvenSums(int n) {
         int sumOdd = n * (2*1 + (n-1) *2) / 2 ; 
         int sumEven = n * (2*2 + (n-1) * 2) / 2; 
-        return gcd(sumOdd, sumEven) ;  
+        return n ;   
     }
 };
