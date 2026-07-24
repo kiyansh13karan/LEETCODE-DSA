2f68cabd051bd5b61d227b3d185569021a9bbd54
Karan Nayal <138805592+kiyansh13karan@users.noreply.github.com>
2026-07-24 23:28:03 +0530

    Time: 19 ms (80.68%) | Memory: 24 MB (39.70%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
-        vector<int> result;
-        if (s.empty() || words.empty()) return result;
+        vector<int> ans;
 
-        int wordLen = words[0].length();
+        if (s.empty() || words.empty())
+            return ans;
+
+        unordered_map<string, int> mp;
+
+        for (string word : words)
+            mp[word]++;
+
+        int wordLen = words[0].size();
         int wordCount = words.size();
         int totalLen = wordLen * wordCount;
 
-        if (s.length() < totalLen) return result;
+        for (int i = 0; i < wordLen; i++) {
+            unordered_map<string, int> window;
+            int left = i, count = 0;
 
-        unordered_map<string,int> freq;
-        for (auto &w : words) freq[w]++;
-
-        for (int offset = 0; offset < wordLen; offset++) {
-
-            int left = offset;
-            int count = 0;
-            unordered_map<string,int> window;
-
-            for (int right = offset; right + wordLen <= s.length(); right += wordLen) {
+            for (int right = i; right + wordLen <= s.size(); right += wordLen) {
 
                 string word = s.substr(right, wordLen);
 
-                if (freq.count(word)) {
+                if (mp.count(word)) {
                     window[word]++;
                     count++;
 
-                    while (window[word] > freq[word]) {
+                    while (window[word] > mp[word]) {
                         string leftWord = s.substr(left, wordLen);
                         window[leftWord]--;
                         left += wordLen;
@@ -35,7 +35,12 @@ public:
                     }
 
                     if (count == wordCount) {
-                        result.push_back(left);
+                        ans.push_back(left);
+
+                        string leftWord = s.substr(left, wordLen);
+                        window[leftWord]--;
+                        left += wordLen;
+                        count--;
                     }
 
                 } else {
@@ -45,6 +50,7 @@ public:
                 }
             }
         }
-        return result;
+
+        return ans;
     }
 };
